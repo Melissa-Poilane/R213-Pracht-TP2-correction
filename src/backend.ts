15681bce3588b43df14b0260998f447a9d645674
pierre pracht <pierre.pracht@gmail.com>
2023-05-03 00:29:34 +0200

    renome backend et export

--- src/backend.ts
+++ src/backend.ts
@@ -1,5 +1,5 @@
 import PocketBase from 'pocketbase'
-const pb = new PocketBase('http://127.0.0.1:8090')
+export const pb = new PocketBase('http://127.0.0.1:8090')
 
 // 5) récupérer la liste de toutes les maison
 
